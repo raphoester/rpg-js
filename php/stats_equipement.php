f5a807d411de6cc3f2b173e1ec129f2b2e796d3e
clementdufour94 <clement.dufourl@gmail.com>
2020-11-27 12:29:46 +0100

    Page de statistique des équipements

--- php/stats_equipement.php
+++ php/stats_equipement.php
@@ -2,11 +2,27 @@
                                           
 <?php
 include("../donnees/data.php");
- $sth = $pdo->query("SELECT * FROM equipement WHERE id_equipement= 1 ");
- 
-
+$sth = $pdo->query("SELECT * FROM equipement WHERE id_personnage= 1 ");
 $result = $sth->fetchAll(PDO::FETCH_OBJ);
 
-echo json_encode($result) ;
+$sth1 = $pdo->query("SELECT * FROM equipement WHERE id_equipement= 1 ");
+$result1 = $sth1->fetchAll(PDO::FETCH_OBJ);
+
+
+
+
+switch($_GET['inventaire']){
+    case "equipement": //Affiche l'inventaire en fonction de l'id du joueur
+        echo json_encode($result);
+    break;
+    case "Stat": //Affiche les caractéristique d'un objet/potion/armure en fonction de l'id de celui ci 
+        echo json_encode($result1);
+    break;
+    default:{
+        echo json_encode("Errreur d'argument !");
+    }
+}
+
+
 
 ?>
