76b36d35dc63daeb59a3d9200553f8d518e690fd
Raphaël Oester <raphael.oester@ynov.com>
2020-11-27 12:14:26 +0100

    boutique 100% fonctionnelle

--- php/boutique.php
+++ php/boutique.php
@@ -4,27 +4,32 @@ define('TAILLE_BOUTIQUE', 6);
 include("../donnees/data.php");
 // $pdo = new PDO("mysql:host=localhost; dbname=rpg_js", "root", "" , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 $tableauEquipement =  $pdo->query("select * from equipement")->fetchAll(); 
+// var_dump($tableauEquipement);
 
 function tirageEquipement($tabEquip){
-    $tabTirage = []; //tirer un élement au hasard dans la base de données
-    foreach ($tabEquip as $element){
-        for ($i = 0; $i > $element["rarete"]; $i++)
+    $tabTirage = array(); //tirer un élement au hasard dans la base de données
+    
+    for ($i = 0 ; $i < count($tabEquip) ; $i++){
+
+        for ($j = 0; $j < intval($tabEquip[$i]["rarete"]); $j++)
         {
-            $tabTirage->append($element);
+            $tabTirage[]=$tabEquip[$i];
         }
     }
-    return $tabTirage[$array_rand($tabTirage)];
+    // var_dump($tabTirage);
+    $rand = rand(0, count($tabTirage)-1);
+
+    return $tabTirage[$rand];
 }
 
-function nouvelleBoutique (){
-    $boutique = [];//créer une nouvelle boutique de 6 objets (tableau)
-    for($i = 0 ; $i < 'TAILLE_BOUTIQUE' ; $i++ ){
-        $boutique.append(tirageEquipement($tableauEquipement));
+
+function nouvelleBoutique ($tabEquip){//créer une nouvelle boutique de 6 objets (tableau)
+    $boutique = array(); 
+    for($i = 0 ; $i < TAILLE_BOUTIQUE ; $i++ ){
+        $boutique[] = tirageEquipement($tabEquip);
     }
     return $boutique;
 }
-
-
 
 switch($_GET['fonction']){
     case "nvBoutique": //obtenir une nouvelle boutique à partir de rien
