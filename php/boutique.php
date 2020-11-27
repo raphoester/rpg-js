5a71fa19c34073dd62c254ba30665900217ac053
Raphaël Oester <raphael.oester@ynov.com>
2020-11-27 11:11:02 +0100

    boutique efficace pour 6 objets

--- php/boutique.php
+++ php/boutique.php
@@ -1,11 +1,12 @@
 <?php 
 
+define(TAILLE_BOUTIQUE, 6);
 include("../donnees/data.php");
 // $pdo = new PDO("mysql:host=localhost; dbname=rpg_js", "root", "" , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 $tableauEquipement =  $pdo->query("select * from equipement")->fetchAll(); 
 
 function tirageEquipement($tabEquip){
-    $tabTirage = [];
+    $tabTirage = []; //tirer un élement au hasard dans la base de données
     foreach ($tabEquip as $element){
         for ($i = 0; $i > $element["rarete"]; $i++)
         {
@@ -14,12 +15,18 @@ function tirageEquipement($tabEquip){
     }
     return $tabTirage[$array_rand($tabTirage)];
 }
-echo tirageEquipement($tableauEquipement);
 
+function nouvelleBoutique (){
+    $boutique = [];//créer une nouvelle boutique de 6 objets (tableau)
+    for($i = 0 ; $i < TAILLE_BOUTIQUE ; $i++ ){
+        $boutique.append(tirageEquipement($tableauEquipement));
+    }
+    return $boutique;
+}
 
 switch($_GET['fonction']){
-    case "nvBoutique":
-        echo json_encode(tirageEquipement($tableauEquipement));
+    case "nvBoutique": //obtenir une nouvelle boutique à partir de rien
+        echo json_encode(nouvelleBoutique($tableauEquipement));
     break;
     default:{
         echo json_encode("Errreur d'argument !");
