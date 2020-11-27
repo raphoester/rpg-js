137d983747cb5e7e38285a257d93093e562f0f7f
Raphaël Oester <raphael.oester@ynov.com>
2020-11-27 16:17:53 +0100

    commentage du code

--- php/boutique.php
+++ php/boutique.php
@@ -6,9 +6,9 @@ include("../donnees/data.php");
 $tableauEquipement =  $pdo->query("select * from equipement where id_joueur = null")->fetchAll(); 
 // var_dump($tableauEquipement);
 
-function tirageEquipement($tabEquip){
-    $tabTirage = array(); //tirer un élement au hasard dans la base de données
+function tirageEquipement($tabEquip){//tirer unéquipement au hasard dans la base de données en respectant les indices de rareté
     
+    $tabTirage = array(); //construction du tableau de tirage en créant une fréquence différenciée artificielle
     for ($i = 0 ; $i < count($tabEquip) ; $i++){
 
         for ($j = 0; $j < intval($tabEquip[$i]["rarete"]); $j++)
@@ -16,13 +16,13 @@ function tirageEquipement($tabEquip){
             $tabTirage[]=$tabEquip[$i];
         }
     }
-    // var_dump($tabTirage);
+    //tirage du nombre aléatoire à partir de la longueur de $tabTirage
     $rand = rand(0, count($tabTirage)-1);
 
     return $tabTirage[$rand];
 }
 
-function nouvelleBoutique ($tabEquip){//créer une nouvelle boutique de 6 objets (tableau)
+function nouvelleBoutique ($tabEquip){//créer une nouvelle boutique de 6 objets (renvoie un tableau)
     $boutique = array(); 
     for($i = 0 ; $i < TAILLE_BOUTIQUE ; $i++ ){
         $boutique[] = tirageEquipement($tabEquip);
@@ -31,8 +31,11 @@ function nouvelleBoutique ($tabEquip){//créer une nouvelle boutique de 6 objets
 }
 
 function acheter($id_equip, $id_perso){
+    //selection de la bonne pièce d'équipement dans la table 
     $donnees_equip=$pdo->query("select * from equipement where id_equipement = "+"$id_equip")->fetch();
+    // le prix de l'équipement est décompté du solde du joueur (aucune vérification - chiffres négatifs autorisés)
     $pdo->exec("update personnage set pO = pO - "+$donnees_equip['prix']+"where id_personnage = $id_perso");
+    //création d'une entrée dans la table relationnelle possède
     $pdo->exec("insert into possede(id_personnage, id_equipement) values($id_equip, $id_perso)");
 }
 
@@ -42,10 +45,10 @@ switch($_GET['fonction']){
     case "nvBoutique": //obtenir une nouvelle boutique à partir de rien
         echo json_encode(nouvelleBoutique($tableauEquipement));
     break;
-    case "acheter":
+    case "acheter"://acheter une pièce d'équipement
         acheter($_GET["id_equip"], $_GET["id_perso"]);
     break;
-    default:{
+    default:{//erreur dans les paramètres GET
         echo json_encode("Errreur d'argument !");
     }
 }?>
