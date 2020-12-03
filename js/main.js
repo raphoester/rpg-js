b9805f3b0057d2a8f92cbf5b9fdc6459ad289a71
Raphaël Oester <raphael.oester@ynov.com>
2020-12-03 12:03:45 +0100

    debut partie js + modif popup

--- js/main.js
+++ js/main.js
@@ -11,39 +11,46 @@ $("#btn_valider").click(function(e){
         success:function(reponse){
             $("#popup_init").css({display:"none"})
             var index_joueur = (JSON.parse(reponse)[0]);
-            lancerPartie(index_joueur);
+            var joueur = chargerJoueur(index_joueur);
+            Partie(joueur);
         }
     });
 })
 
-
-
-function lancerPartie(index_joueur){
+function chargerJoueur(index_joueur){
     $.ajax({
         url:"php/personnage.php?fonction=afficherInfos",
         data: "id_perso="+index_joueur, 
         dataType:"json",
         success:function(reponse){
-            var joueur = JSON.parse(reponse);
-            // console.log(joueur);
+            return joueur;    
         }
     })
+    return null;
 }
 
 
+function Partie(joueur){
+    while (partieEnCours == true){
+        partieEnCours = boutique(joueur);
+        partieEnCours = combat(joueur);
+    }
+}
 
 
-// partieEnCours = true;
+function boutique(joueur){
 
-// function jouer(){
-//     while (partieEnCours){
-//         combatEnCours = true;
-//         while (combatEnCours)
-//         {
-//             //combat... un tour de boucle par tour de jeu
-//             //le joueur attaque 
-//             //le monstre attaque
-    
-//         }
-//     }
-// }
+}
+
+function combat(monstre, joueur){
+    while (partieEnCours){
+        combatEnCours = true;
+        while (combatEnCours)
+        {
+            //combat... un tour de boucle par tour de jeu
+            //le joueur attaque 
+            //le monstre attaque
+            
+        }
+    }
+}
